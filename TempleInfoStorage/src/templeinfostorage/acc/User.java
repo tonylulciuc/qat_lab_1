1b4aefe3d3da87175747e1861166062ee366a311
Antony Lulciuc <tonylulciuc@gmail.com>
2016-09-03 23:53:34 -0400

    +Added user interface

---
+++ TempleInfoStorage/src/templeinfostorage/acc/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templeinfostorage.acc;
+
+import templeinfostorage.acc.properties.UserInfo;
+
+/**
+ *
+ * @author tonyl
+ */
+public interface User {
+    
+    /**
+     * Getter Methods
+     */
+    
+    /**
+     * Returns a non-mutable instance of user information
+     * @return 
+     */
+    UserInfo getUserInfo();
+    
+    /**
+     * Setter Methods
+     */
+   
+    /**
+     * Modifies user information
+     * @param _userInfo 
+     */
+    void setUserInfo(UserInfo _userInfo);
+}
